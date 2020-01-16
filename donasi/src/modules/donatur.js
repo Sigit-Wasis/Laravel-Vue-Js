5556aae20e3533bfc980182c6c89138af86c469f
Sigit-Wasis <sigitghoticmetal2001@gmail.com>
2020-01-16 11:00:28 +0700

    module & helpers

---
+++ donasi/src/modules/donatur.js
@@ -0,0 +1,19 @@
+const donatur = {
+    namespaced: true,
+    state: {
+        listDonatur: [
+            { name: 'Fulan A' },
+            { name: 'Fulan B' },
+            { name: 'Fulan C' },
+            { name: 'Fulan D' }
+        ]
+    },
+    mutations: {
+        // NONE
+    },
+    actions: {
+        // NONE
+    }
+}
+
+export default donatur
